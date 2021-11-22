38e7ffcaf736c987bf75ceda36008e239612e3fc
Alexander Yaskevich <yaskevich.san@mail.ru>
2021-11-22 18:51:48 +0300

    Move to postgres and add docker-compose

--- mybank/mybank/settings.py
+++ mybank/mybank/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '9d#6$nbbnc6ea2y(wxf1q=gjp$t8c9#bqj&_=csndevz^r8s#0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -84,9 +84,17 @@ WSGI_APPLICATION = 'mybank.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
